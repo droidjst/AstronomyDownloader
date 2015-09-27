4037b5b47069d93ad70a7a29a40f8a0e07db7c19
Joe <droidjst@gmail.com>
2015-09-26 21:30:47 -0400

    Project Constants
    This includes constants used throughout the program.

---
+++ src/com/droidjst/Const.java
@@ -0,0 +1,32 @@
+
+/*
+    Copyright 2015 Joseph Tranquillo {name of copyright owner}
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+
+package com.droidjst.apoddownload;
+
+import java.io.File;
+
+public class Const
+{
+	public final static String URL_NASA_APOD = "http://apod.nasa.gov/apod/";
+	
+	public final static String DB_NAME = "apod.db";
+	
+	public final static String FILE_SEP = File.separator;
+	
+	public final static String ARCHIVE_DIR = FILE_SEP + "archive" + FILE_SEP;
+}
