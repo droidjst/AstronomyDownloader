64d07be61167f2a3a85ce105ba7dcc32e4326663
Joe <droidjst@gmail.com>
2015-09-26 21:48:40 -0400

    The Driver
    The first run will create the apod.db file.
    Second run will begin to add the image url, image credit, and image
    explanation for each item in the database.

---
+++ src/com/droidjst/Driver.java
@@ -0,0 +1,92 @@
+package com.droidjst.apoddownload;
+
+import java.io.File;
+
+public class Driver
+{
+	public static void main(String[] args)
+	{
+		Database database = new Database();
+		
+		if(database.isAvailable() == false)
+		{
+			boolean created = database.createSQLiteTable();
+			
+			if(created)
+			{
+				DatabaseUtil dbutil = new DatabaseUtil();
+				
+				boolean completed = dbutil.addBasicArchive();
+				
+				if(completed)
+				{
+					System.out.println("entires= " + database.getCount());
+				}
+				else
+				{
+					// unable to add archive to database
+				}
+			}
+			else
+			{
+				// unable to create database
+			}
+		}
+		else
+		{
+			DatabaseUtil dbutil = new DatabaseUtil();
+			
+			JsoupUtil jsouputil = new JsoupUtil();
+			
+			File archive_dir = new File(new File("").getAbsolutePath() + Const.ARCHIVE_DIR);
+			
+			if(archive_dir.exists() == false)
+			{
+				archive_dir.mkdirs();
+			}
+			
+			String[] urls = dbutil.getURLs();
+			
+			String html;
+			
+			String image_src;
+			String credit;
+			String explanation;
+			
+			int index = 258;
+			
+			String url;
+			
+			while(index < urls.length)
+			{
+				url = urls[index];
+				
+				/*
+				 * html = jsouputil.savePage(Const.URL_NASA_APOD + url, url);
+				 */
+				
+				html = jsouputil.getHTML(Const.URL_NASA_APOD + url);
+				
+				if(html == null)
+				{
+					System.out.println("url data null= " + url);
+					
+					continue;
+				}
+				
+				image_src = HTMLUtil.getImageSource(html);
+				credit = HTMLUtil.getImageCredit(html);
+				explanation = HTMLUtil.getExplanation(html);
+				
+				System.out.println("--------");
+				
+				dbutil.addExtendedArchiveItem(image_src, url.substring(2, 8), credit, explanation);
+				
+				index++;
+			}
+			
+		}
+	}
+	
+	
+}
